8acce027c9fd16397a78e5850c625ddd6b9147e9
davide-marchiori <mrcdvd77@gmail.com>
2023-11-22 07:27:47 +0100

    Revert "introduced analytics"
    This reverts commit 2e32d956bdbfb178305ed672fc4e7c855743b553.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Analytics />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
